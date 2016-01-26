f386cee2422ce8f0dfd0c5e585acadf5587a0f6b
revesansparole <revesansparole@gmail.com>
2016-01-26 08:14:36 +0100

    more

--- src/toto/cli.py
+++ src/toto/cli.py
@@ -2,6 +2,11 @@ from sys import argv
 
 
 def main():
+    """Example script, print stuff, return passed argument
+    If one exists on the command line.
+
+    Author: myself
+    """
     if len(argv) > 1:
         a = int(argv[1])
     else:
@@ -10,4 +15,3 @@ def main():
     print "execution script"
     print "a", a
     return a * 10
-
