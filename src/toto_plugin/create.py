e169ee5d11f4e8164f51a31f6d8cf7639fd8a026
revesansparole <revesansparole@gmail.com>
2016-02-07 22:30:31 +0100

    added plugin

---
+++ src/toto_plugin/create.py
@@ -0,0 +1,9 @@
+import json
+
+p1 = dict(name="read",
+          module="testio",
+          function="read",
+          info1="toto was here")
+
+with open("testio.json", 'w') as f:
+    json.dump(p1, f, sort_keys=True, indent=4)
