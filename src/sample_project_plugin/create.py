c761967c640cd655602b4ab5e4b1ea86a5250d40
revesansparole <revesansparole@gmail.com>
2016-02-11 07:52:22 +0100

    added workflow

--- src/sample_project_plugin/create.py
+++ src/sample_project_plugin/create.py
@@ -4,7 +4,16 @@ p1 = dict(name="read",
           category="oanode",
           module="testio",
           function="read",
-          info1="toto was here")
+          description="toto was here")
 
 with open("testio.json", 'w') as f:
     json.dump(p1, f, sort_keys=True, indent=4)
+
+
+p2 = dict(name="workflow",
+          category="oaworkflow",
+          description="compare binarization algorithms")
+
+with open("workflow.json", 'w') as f:
+    json.dump(p2, f, sort_keys=True, indent=4)
+
