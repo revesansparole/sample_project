b24074b43951c50ece12b97d2718c323ef741f99
revesansparole <revesansparole@gmail.com>
2016-02-15 18:48:21 +0100

    uuid in node and workflows

---
+++ src/sample_project/sample_script.py
@@ -0,0 +1,18 @@
+"""Example script for tutorial purpose.
+"""
+
+import os
+
+
+def my_script(txt="beau texte"):
+    """Print txt message and return it.
+
+    Args:
+        txt: (str) msg to display
+
+    Returns:
+        (str) return txt
+    """
+    print(txt)
+
+    return txt
--- src/sample_project_plugin/create.py
+++ src/sample_project_plugin/create.py
@@ -1,33 +1,72 @@
 import json
+from uuid import uuid1
 
-p1 = dict(name="read",
+
+n1 = dict(id=uuid1().int,
+          name="read",
           category="oanode",
-          description="toto was here",
+          description="read data from resource",
           author="revesansparole",
           function="testio:read",
-          inputs=[dict(name="in1", interface="IInt",
-                       value="0", descr="counter"),
-                  dict(name="in2", interface="IStr",
-                       value="a", descr="unit")],
-          outputs=[dict(name="ret", interface="IInt",
-                        descr="important result")])
+          inputs=[dict(name="url", interface="IURL",
+                       value="", descr="URL of resource to read")],
+          outputs=[dict(name="ret", interface="any",
+                        descr="red data")])
 
-with open("testio.json", 'w') as f:
-    json.dump(p1, f, sort_keys=True, indent=4)
-
-p2 = dict(name="workflow",
-          category="oaworkflow",
-          description="compare binarization algorithms",
+n2 = dict(id=uuid1().int,
+          name="int",
+          category="oanode",
+          description="Convert number to int",
           author="revesansparole",
-          nodes=[("read", 100, 10),
-                 ("node2", 200, 10),
-                 ("node3", 150, 100),
-                 ("print", 150, 200)],
-          connections=[(0, "res", 2, "in1"),
-                       (1, "res", 2, "in2"),
-                       (2, "res", 3, "in")])
+          function="testio:int",
+          inputs=[dict(name="val", interface="any",
+                       value=0, descr="Value to convert")],
+          outputs=[dict(name="ret", interface="IInt",
+                        descr="input converted to int")])
+
+n3 = dict(id=uuid1().int,
+          name="plus",
+          category="oanode",
+          description="add two numbers",
+          author="revesansparole",
+          function="testio:plus",
+          inputs=[dict(name="in1", interface="IInt",
+                       value=0, descr="first value"),
+                  dict(name="in2", interface="IInt",
+                       value=0, descr="second value")],
+          outputs=[dict(name="ret", interface="IInt",
+                        descr="result")])
+
+n4 = dict(id=uuid1().int,
+          name="print",
+          category="oanode",
+          description="print value",
+          author="revesansparole",
+          function="testio:print",
+          inputs=[dict(name="in", interface="any",
+                       value="", descr="anything")],
+          outputs=[dict(name="out", interface="any",
+                        descr="just passing in out")])
+
+
+for ndef in (n1, n2, n3, n4):
+    with open("ndef_%s.json" % ndef["name"], 'w') as f:
+        json.dump(ndef, f, sort_keys=True, indent=4)
+
+wkf = dict(id=uuid1().int,
+           name="workflow",
+           category="oaworkflow",
+           description="compare binarization algorithms",
+           author="revesansparole",
+           nodes=[(n1['id'], None, 100, 10),
+                  (n2['id'], "node2", 200, 10),
+                  (n3['id'], None, 150, 100),
+                  (n4['id'], None, 150, 200)],
+           connections=[(0, "ret", 2, "in1"),
+                        (1, "ret", 2, "in2"),
+                        (2, "ret", 3, "in")])
 
 
 with open("workflow.json", 'w') as f:
-    json.dump(p2, f, sort_keys=True, indent=4)
+    json.dump(wkf, f, sort_keys=True, indent=4)
 
